8dafa550b6e16ea2f6a9f9d779ea2ab0e8111d27
曹超 <chaochao.cao@yinyuetai.com>
2019-04-09 17:39:31 +0800

    textLink

---
+++ HuanLiDemo/Cells/TextLinkTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  TextLinkTableViewCell.h
+//  HuanLiDemo
+//
+//  Created by cc on 2019/4/3.
+//  Copyright © 2019 HuanLiDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TextLinkTableViewCell : UITableViewCell
++ (CGFloat)cellHeight;
+@end
+
+NS_ASSUME_NONNULL_END
